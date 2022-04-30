fc88aa87190c59710409028bb406374217db5a6d
olayiwola1995 <olayiwolarahmon84@gmail.com>
2022-04-29 21:47:02 -0700

    new project

---
+++ src/components/header/header.data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    path: 'home',
+    label: 'Home',
+  },
+  {
+    path: 'feature',
+    label: 'Features',
+  },
+  {
+    path: 'pricing', 
+    label: 'Products',
+  },
+  {
+    path: 'testimonial',
+    label: 'Testimonial',
+  },
+  {
+    path: {
+      url:'https://wa.me/2348067317325'},
+    label:'Buy Now'
+  }
+];
